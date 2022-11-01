c7d6b5310927b2376ad548b628152d61be029e62
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-10-31 21:27:03 -0400

    Migrate chat commands to TextChatService commands

--- src/server/ChatSpawn.server.lua
+++ src/server/ChatSpawn.server.lua
@@ -1,72 +1,70 @@
 local Players = game:GetService("Players")
 local ServerScriptService = game:GetService("ServerScriptService")
+local TextChatService = game:GetService("TextChatService")
 
 local CRAFT_RADIUS = 128
 
 local Object = require(ServerScriptService:WaitForChild("Object"))
 local Crafting = require(ServerScriptService:WaitForChild("Crafting"))
 
--- TODO: Cmdr
-local commands = {
-	["/s"] = function(player: Player, objectName: string, amount: number)
-		amount = if amount then tonumber(amount) or 1 else 1
-		if amount ~= amount then
-			amount = 1
-		end
-
-		local character = player.Character
-		if not character then
-			return
-		end
-
-		print("Spawn", objectName, amount)
-		objectName = Object.fuzzySearch(objectName)
-		Crafting:Spawn({
-			{
-				Resource = objectName;
-				Amount = math.clamp(amount, 1, 10);
-			}
-		}, character:GetPivot() * CFrame.new(0, 0, -5))
-	end;
-	["/c"] = function(player: Player, recipeName: string, amount: number)
-		amount = if amount then tonumber(amount) or 1 else 1
-		if amount ~= amount then
-			amount = 1
-		end
-
-		local character = player.Character
-		if not character then
-			return
-		end
-		local cframe = character:GetPivot()
-
-		print("Craft", recipeName, amount)
-		local recipe = Crafting.getRecipe(Crafting.fuzzySearch(recipeName))
-		local cost = #recipe.Ingredients
-		if cost == 0 then
-			amount = math.clamp(amount, 1, 10)
-		end
-
-		local resultCount = 0
-		while resultCount < amount do
-			local results = assert(Crafting:CraftRecipe(cframe, recipe, CRAFT_RADIUS, player.UserId), "Not enough resources to craft.")
-			resultCount += #results
-		end
-	end;
-}
-
-local function playerAdded(player)
-	player.Chatted:Connect(function(message)
-		for command, action in pairs(commands) do
-			local arguments = string.split(string.gsub(message, "%s+", " "), " ")
-			if arguments[1] == command then
-				action(player, unpack(arguments, 2))
-			end
-		end
+local function makeCommand(primaryAlias: string, secondaryAlias: string?, action: (player: Player, ...string) -> ())
+	local command = Instance.new("TextChatCommand")
+	command.PrimaryAlias = primaryAlias
+	if secondaryAlias then
+		command.SecondaryAlias = secondaryAlias
+	end
+	command.Triggered:Connect(function(originTextSource: TextSource, unfilteredText: string)
+		local player = assert(Players:GetPlayerByUserId(originTextSource.UserId), string.format("Cannot execute command. No player for UserId: %d", originTextSource.UserId))
+		local arguments = string.split(string.gsub(unfilteredText, "%s+", " "), " ")
+		action(player, unpack(arguments, 2))
 	end)
+	command.Parent = TextChatService
 end
 
-for _, player in ipairs(Players:GetPlayers()) do
-	task.spawn(playerAdded, player)
-end
-Players.PlayerAdded:Connect(playerAdded)
+-- TODO: Cmdr
+makeCommand("/spawn", "/s", function(player: Player, objectName: string, _amount: string)
+	local amount: number = if _amount then tonumber(_amount) or 1 else 1
+	if amount ~= amount then
+		amount = 1
+	end
+
+	local character = player.Character
+	if not character then
+		return
+	end
+
+	print("Spawn", objectName, amount)
+	objectName = Object.fuzzySearch(objectName)
+	Crafting:Spawn({
+		{
+			Resource = objectName;
+			Amount = math.clamp(amount, 1, 10);
+		}
+	}, character:GetPivot() * CFrame.new(0, 0, -5))
+end)
+
+makeCommand("/craft", "/c", function(player: Player, recipeName: string, _amount: string)
+	local amount: number = if _amount then tonumber(_amount) or 1 else 1
+	if amount ~= amount then
+		amount = 1
+	end
+
+	local character = player.Character
+	if not character then
+		return
+	end
+	local cframe = character:GetPivot()
+
+	print("Craft", recipeName, amount)
+	local recipe = Crafting.getRecipe(Crafting.fuzzySearch(recipeName))
+	local cost = #recipe.Ingredients
+	if cost == 0 then
+		amount = math.clamp(amount, 1, 10)
+	end
+
+	local resultCount = 0
+	while resultCount < amount do
+		local results = assert(Crafting:CraftRecipe(cframe, recipe, CRAFT_RADIUS, player.UserId), "Not enough resources to craft.")
+		resultCount += #results
+	end
+end)
